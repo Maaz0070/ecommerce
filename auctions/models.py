0e5e72534f5b0894f8bdd9e3744dbcea502fe870
Maaz Adil <maazadil786@gmail.com>
2021-02-24 23:21:45 -0800

    first commit

---
+++ auctions/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+#from django.contrib.auth import views
+
+
+class User(AbstractUser):
+    pass
+    #bids = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="publisher")
+    
+    def __str__(self):
+        return f"{self.name}"
+
+class Bid(models.Model):
+    price = models.IntegerField() 
+    num = models.IntegerField()
+    #time = models.DateField()
+    highestBidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="winner")
+
+    def __str__(self):
+        return f"${self.price}: with {self.num} bids and highest bidder is {self.highestBidder} with {self.time} remaining"
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=64)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="publisher")
+    
+
+    def __str__(self):
+        return f"{self.publisher}: {self.comment} for {self.listing}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100000)
+    image = models.URLField(max_length=2000)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    currentBid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="bid")
+    comments = models.ManyToManyField(Comment, blank=True, related_name="post_comments")
+
+
+    def __str__(self):
+        return f"{self.title}: {self.description} by {self.seller} at {self.currentBid}"
+
+
+
---
+++ auctions/views.py
@@ -0,0 +1,89 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
+from .models import User, Listing
+
+
+def index(request):
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
+    
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+        username = User()
+    else: #if user didnt just fill form they need to go to login page to do that
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST": #if they just filled out register form
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user) #logs in user 
+        return HttpResponseRedirect(reverse("index")) #takes to index
+    else:
+        return render(request, "auctions/register.html") #if they click register
+
+@login_required
+def newListing(request):
+    if request.method == "POST": #if they just enetered values for new listing
+        title = request.POST["title"]
+        description = request.POST["description"]
+        bid = request.POST["bid"]
+        image = request.POST["photo"]
+        category = request.POST["category"]
+    
+    return render(request, "auctions/new.html") #right when they cllick the button
+
+def listing(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    return render(request, "auctions/listing.html", {
+        "title": title,
+        "description": description,
+        "image": image,
+        "seller": seller,
+        "bid": currentBid
+    })
